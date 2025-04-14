1cf9e984b4cc1d145445c584f3e7ed947fa75da9
Miguel de Leon <miguelangelodeleon.12@gmail.com>
2025-04-14 10:04:33 +0200

    feat(be): getProjectById

--- apps/backend/src/controllers/projects.controller.ts
+++ apps/backend/src/controllers/projects.controller.ts
@@ -1,6 +1,6 @@
 import { type Express, type Request, type Response } from "express";
 import { authenticateToken } from "../middleware/authMiddleware";
-import { getProjects } from "../repository/projects.repository";
+import { getProjectById, getProjects } from "../repository/projects.repository";
 
 export const createProjectsController = (app: Express) => {
   app.get(
@@ -9,6 +9,24 @@ export const createProjectsController = (app: Express) => {
     async (req: Request, res: Response) => {
       try {
         const projects = await getProjects();
+
+        if (!projects) return res.status(404).send("projects not found");
+
+        return res.status(200).send(projects);
+      } catch (error) {
+        console.error(error);
+        return res.status(500);
+      }
+    },
+  );
+
+  app.get(
+    "/projects/:id",
+    authenticateToken,
+    async (req: Request, res: Response) => {
+      try {
+        const projectId = req.params.id;
+        const projects = await getProjectById(projectId);
 
         if (!projects) return res.status(404).send("projects not found");
 
--- apps/backend/src/repository/projects.repository.ts
+++ apps/backend/src/repository/projects.repository.ts
@@ -32,3 +32,41 @@ export async function getProjects(): Promise<IProject[]> {
     },
   }));
 }
+
+export async function getProjectById(id: string): Promise<IProject | null> {
+  const res = await sql`
+    SELECT 
+      p.id AS project_id,
+      p.created_at AS project_created_at,
+      p.name AS project_name,
+      p.client,
+      p.description,
+      p.start_date,
+      p.end_date,
+      s.id AS status_id,
+      s.name AS status_name
+    FROM public."PROJECTS" p
+    JOIN public."STATUSES" s ON p.status_id = s.id
+    WHERE p.id = ${id}
+      AND s.name IS DISTINCT FROM p.name
+    LIMIT 1
+  `;
+
+  if (res.length === 0) return null;
+
+  const row = res[0];
+
+  return {
+    id: row.project_id,
+    created_at: new Date(row.project_created_at),
+    name: row.project_name,
+    client: row.client,
+    description: row.description,
+    start_date: new Date(row.start_date),
+    end_date: new Date(row.end_date),
+    status: {
+      id: row.status_id,
+      name: row.status_name,
+    },
+  };
+}
