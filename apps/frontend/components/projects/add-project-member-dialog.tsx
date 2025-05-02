f9eed3c5f8d83cb9e8ae291976d3b09e7d0176f8
niccolonaso <niccolonaso@yahoo.it>
2025-05-02 15:37:00 +0200

    fix: show only available users to assign at the project

--- apps/frontend/components/projects/add-project-member-dialog.tsx
+++ apps/frontend/components/projects/add-project-member-dialog.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useSessionContext } from "@/app/utenti/SessionData";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { motion } from "framer-motion";
@@ -10,8 +9,10 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 import { useCreateAllocation } from "@/api/useCreateAllocation";
+import { useGetProjectsUsersAvailable } from "@/api/useGetProjectsUsersAvailable";
 import { format } from "date-fns";
 import { DatePicker } from "../custom/DatePicker";
+import { Loader } from "../custom/Loader";
 import {
   Command,
   CommandEmpty,
@@ -50,7 +51,7 @@ export function AddProjectMemberDialog({
   projectId: string;
   refetch: () => void;
 }>) {
-  const { users } = useSessionContext();
+  const { users, isLoading } = useGetProjectsUsersAvailable(projectId);
   const [isPopoverOpen, setIsPopoverOpen] = useState(false);
   const { createAllocation } = useCreateAllocation();
 
@@ -66,192 +67,201 @@ export function AddProjectMemberDialog({
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent className="sm:max-w-[600px]">
-        <DialogHeader>
-          <DialogTitle>{"Aggiungi membro al progetto"}</DialogTitle>
-        </DialogHeader>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <>
+            <DialogHeader>
+              <DialogTitle>{"Aggiungi membro al progetto"}</DialogTitle>
+            </DialogHeader>
 
-        <Form {...form}>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              form.handleSubmit(() => {
-                const values = form.getValues();
+            <Form {...form}>
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  form.handleSubmit(() => {
+                    const values = form.getValues();
 
-                createAllocation({
-                  project_id: projectId,
-                  start_date: values.start_date
-                    ? format(values.start_date, "yyyy-MM-dd")
-                    : undefined,
-                  end_date: values.end_date
-                    ? format(values.end_date, "yyyy-MM-dd")
-                    : undefined,
-                  percentage: values.percentage,
-                  user_id: values.user_id,
-                }).then(() => {
-                  onOpenChange(false);
-                  form.reset();
-                  refetch();
-                });
-              })(e);
-            }}
-            className="space-y-4"
-          >
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <div className="col-span-1">
-                <FormField
-                  control={form.control}
-                  name="user_id"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Nome Membro</FormLabel>
-                      <FormControl>
-                        <Popover
-                          open={isPopoverOpen}
-                          onOpenChange={setIsPopoverOpen}
-                        >
-                          <PopoverTrigger asChild>
-                            <Button
-                              variant="outline"
-                              role="combobox"
-                              aria-expanded={open}
-                              className="w-[250px] justify-between"
+                    createAllocation({
+                      project_id: projectId,
+                      start_date: values.start_date
+                        ? format(values.start_date, "yyyy-MM-dd")
+                        : undefined,
+                      end_date: values.end_date
+                        ? format(values.end_date, "yyyy-MM-dd")
+                        : undefined,
+                      percentage: values.percentage,
+                      user_id: values.user_id,
+                    }).then(() => {
+                      onOpenChange(false);
+                      form.reset();
+                      refetch();
+                    });
+                  })(e);
+                }}
+                className="space-y-4"
+              >
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                  <div className="col-span-1">
+                    <FormField
+                      control={form.control}
+                      name="user_id"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Nome Membro</FormLabel>
+                          <FormControl>
+                            <Popover
+                              open={isPopoverOpen}
+                              onOpenChange={setIsPopoverOpen}
                             >
-                              {field.value
-                                ? users.find((user) => user.id === field.value)
-                                    ?.name
-                                : "Seleziona membro..."}
-                              <ChevronsUpDown className="opacity-50" />
-                            </Button>
-                          </PopoverTrigger>
-                          <PopoverContent className="w-[250px] p-0">
-                            <Command>
-                              <CommandInput
-                                placeholder="Seleziona membro..."
-                                className="h-9 pointer-events-auto"
-                              />
-                              <CommandList>
-                                <CommandEmpty>No framework found.</CommandEmpty>
-                                <CommandGroup>
-                                  {users.map((user) => (
-                                    <CommandItem
-                                      key={user.id}
-                                      value={user.name}
-                                      className="pointer-events-auto"
-                                      onSelect={() => {
-                                        field.onChange(user.id);
-                                        setIsPopoverOpen(false);
-                                      }}
-                                    >
-                                      {user.name}
-                                      <Check
-                                        className={cn(
-                                          "ml-auto",
-                                          field.value === user.id
-                                            ? "opacity-100"
-                                            : "opacity-0",
-                                        )}
-                                      />
-                                    </CommandItem>
-                                  ))}
-                                </CommandGroup>
-                              </CommandList>
-                            </Command>
-                          </PopoverContent>
-                        </Popover>
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
+                              <PopoverTrigger asChild>
+                                <Button
+                                  variant="outline"
+                                  role="combobox"
+                                  aria-expanded={open}
+                                  className="w-[250px] justify-between"
+                                >
+                                  {field.value
+                                    ? users.find(
+                                        (user) => user.id === field.value,
+                                      )?.name
+                                    : "Seleziona membro..."}
+                                  <ChevronsUpDown className="opacity-50" />
+                                </Button>
+                              </PopoverTrigger>
+                              <PopoverContent className="w-[250px] p-0">
+                                <Command>
+                                  <CommandInput
+                                    placeholder="Seleziona membro..."
+                                    className="h-9 pointer-events-auto"
+                                  />
+                                  <CommandList>
+                                    <CommandEmpty>
+                                      No framework found.
+                                    </CommandEmpty>
+                                    <CommandGroup>
+                                      {users.map((user) => (
+                                        <CommandItem
+                                          key={user.id}
+                                          value={user.name}
+                                          className="pointer-events-auto"
+                                          onSelect={() => {
+                                            field.onChange(user.id);
+                                            setIsPopoverOpen(false);
+                                          }}
+                                        >
+                                          {user.name}
+                                          <Check
+                                            className={cn(
+                                              "ml-auto",
+                                              field.value === user.id
+                                                ? "opacity-100"
+                                                : "opacity-0",
+                                            )}
+                                          />
+                                        </CommandItem>
+                                      ))}
+                                    </CommandGroup>
+                                  </CommandList>
+                                </Command>
+                              </PopoverContent>
+                            </Popover>
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
 
-              <div className="col-span-1">
-                <FormField
-                  control={form.control}
-                  name="percentage"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Percentuale allocazione</FormLabel>
-                      <FormControl>
-                        <Input
-                          placeholder="Nome del cliente"
-                          type="number"
-                          className="w-[250px]"
-                          {...field}
-                          onChange={(e) => {
-                            if (Number(e.target.value) > 100)
-                              field.onChange(100);
-                            else if (Number(e.target.value) < 5)
-                              field.onChange(5);
-                            else field.onChange(Number(e.target.value));
-                          }}
-                          max={100}
-                          min={5}
-                          step={5}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div className="col-span-1">
-                <FormField
-                  control={form.control}
-                  name="start_date"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Data inizio</FormLabel>
-                      <FormControl>
-                        <DatePicker
-                          {...field}
-                          date={field.value}
-                          setDate={field.onChange}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div className="col-span-1">
-                <FormField
-                  control={form.control}
-                  name="end_date"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Data Fine</FormLabel>
-                      <FormControl>
-                        <DatePicker
-                          {...field}
-                          date={field.value}
-                          setDate={field.onChange}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-            </div>
+                  <div className="col-span-1">
+                    <FormField
+                      control={form.control}
+                      name="percentage"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Percentuale allocazione</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="Nome del cliente"
+                              type="number"
+                              className="w-[250px]"
+                              {...field}
+                              onChange={(e) => {
+                                if (Number(e.target.value) > 100)
+                                  field.onChange(100);
+                                else if (Number(e.target.value) < 5)
+                                  field.onChange(5);
+                                else field.onChange(Number(e.target.value));
+                              }}
+                              max={100}
+                              min={5}
+                              step={5}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <div className="col-span-1">
+                    <FormField
+                      control={form.control}
+                      name="start_date"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Data inizio</FormLabel>
+                          <FormControl>
+                            <DatePicker
+                              {...field}
+                              date={field.value}
+                              setDate={field.onChange}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <div className="col-span-1">
+                    <FormField
+                      control={form.control}
+                      name="end_date"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Data Fine</FormLabel>
+                          <FormControl>
+                            <DatePicker
+                              {...field}
+                              date={field.value}
+                              setDate={field.onChange}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                </div>
 
-            <DialogFooter>
-              <Button
-                type="button"
-                variant="outline"
-                onClick={() => onOpenChange(false)}
-              >
-                Annulla
-              </Button>
-              <motion.div
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.95 }}
-              >
-                <Button type="submit">Aggiungi</Button>
-              </motion.div>
-            </DialogFooter>
-          </form>
-        </Form>
+                <DialogFooter>
+                  <Button
+                    type="button"
+                    variant="outline"
+                    onClick={() => onOpenChange(false)}
+                  >
+                    Annulla
+                  </Button>
+                  <motion.div
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.95 }}
+                  >
+                    <Button type="submit">Aggiungi</Button>
+                  </motion.div>
+                </DialogFooter>
+              </form>
+            </Form>
+          </>
+        )}
       </DialogContent>
     </Dialog>
   );
