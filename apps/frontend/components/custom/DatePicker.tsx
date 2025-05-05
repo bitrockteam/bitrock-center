bf4acdc5997e097dac575c1172000a205dae3449
Gianluca La Rosa <gianluca_larosa@hotmail.com>
2025-05-05 17:39:04 +0200

    feat: add reset date

--- apps/frontend/components/custom/DatePicker.tsx
+++ apps/frontend/components/custom/DatePicker.tsx
@@ -1,16 +1,14 @@
 "use client";
 
 import { format } from "date-fns";
-import { CalendarIcon } from "lucide-react";
-
-import { Button } from "@/components/ui/button";
+import { CalendarIcon, XIcon } from "lucide-react";
 import { Calendar } from "@/components/ui/calendar";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import { cn } from "@/lib/utils";
+import {Input} from "@/components/ui/input";
 
 export function DatePicker({
   date,
@@ -24,16 +22,14 @@ export function DatePicker({
   return (
     <Popover>
       <PopoverTrigger asChild>
-        <Button
-          variant={"outline"}
-          className={cn(
-            "w-[250px] justify-start text-left font-normal",
-            !date && "text-muted-foreground",
-          )}
-        >
-          <CalendarIcon />
-          {date ? format(date, "PPP") : <span>Pick a date</span>}
-        </Button>
+        <div className="relative">
+          <Input value={date ? format(date, "dd/MM/yyyy") : "Seleziona una data"} className="pl-10"/>
+          <CalendarIcon className="absolute top-[6px] left-2 cursor-pointer scale-80"/>
+          { date && <XIcon className="absolute top-[6px] right-2 cursor-pointer scale-85" onClick={(evt) => {
+            evt.stopPropagation()
+            setDate(undefined)
+          }}/> }
+        </div>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0" align="start">
         <Calendar
