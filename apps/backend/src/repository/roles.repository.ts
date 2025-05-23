cf989c7b8c87e363825331b396972b1e51c181c5
niccolonaso <niccolonaso@yahoo.it>
2025-05-24 00:39:21 +0200

    feat: prisma setup

--- apps/backend/src/repository/roles.repository.ts
+++ apps/backend/src/repository/roles.repository.ts
@@ -1,5 +1,6 @@
 import { IRole } from "@bitrock/types";
 import { sql } from "../config/postgres";
+import prisma from "../prisma";
 
 export async function getRoles() {
   const res = await sql`SELECT * FROM public."ROLES"`;
