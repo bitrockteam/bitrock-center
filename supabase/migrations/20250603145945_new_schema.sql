6b4991719467022441a976edffdc23ceae8dc5f1
Davide Ghiotto <dghiotto.dev@gmail.com>
2025-06-03 17:06:36 +0200

    feat: migrations file

---
+++ supabase/migrations/20250603145945_new_schema.sql
@@ -0,0 +1,505 @@
+revoke delete on table "public"."ALLOCATIONS" from "anon";
+
+revoke insert on table "public"."ALLOCATIONS" from "anon";
+
+revoke references on table "public"."ALLOCATIONS" from "anon";
+
+revoke select on table "public"."ALLOCATIONS" from "anon";
+
+revoke trigger on table "public"."ALLOCATIONS" from "anon";
+
+revoke truncate on table "public"."ALLOCATIONS" from "anon";
+
+revoke update on table "public"."ALLOCATIONS" from "anon";
+
+revoke delete on table "public"."ALLOCATIONS" from "authenticated";
+
+revoke insert on table "public"."ALLOCATIONS" from "authenticated";
+
+revoke references on table "public"."ALLOCATIONS" from "authenticated";
+
+revoke select on table "public"."ALLOCATIONS" from "authenticated";
+
+revoke trigger on table "public"."ALLOCATIONS" from "authenticated";
+
+revoke truncate on table "public"."ALLOCATIONS" from "authenticated";
+
+revoke update on table "public"."ALLOCATIONS" from "authenticated";
+
+revoke delete on table "public"."ALLOCATIONS" from "service_role";
+
+revoke insert on table "public"."ALLOCATIONS" from "service_role";
+
+revoke references on table "public"."ALLOCATIONS" from "service_role";
+
+revoke select on table "public"."ALLOCATIONS" from "service_role";
+
+revoke trigger on table "public"."ALLOCATIONS" from "service_role";
+
+revoke truncate on table "public"."ALLOCATIONS" from "service_role";
+
+revoke update on table "public"."ALLOCATIONS" from "service_role";
+
+revoke delete on table "public"."PERMITS" from "anon";
+
+revoke insert on table "public"."PERMITS" from "anon";
+
+revoke references on table "public"."PERMITS" from "anon";
+
+revoke select on table "public"."PERMITS" from "anon";
+
+revoke trigger on table "public"."PERMITS" from "anon";
+
+revoke truncate on table "public"."PERMITS" from "anon";
+
+revoke update on table "public"."PERMITS" from "anon";
+
+revoke delete on table "public"."PERMITS" from "authenticated";
+
+revoke insert on table "public"."PERMITS" from "authenticated";
+
+revoke references on table "public"."PERMITS" from "authenticated";
+
+revoke select on table "public"."PERMITS" from "authenticated";
+
+revoke trigger on table "public"."PERMITS" from "authenticated";
+
+revoke truncate on table "public"."PERMITS" from "authenticated";
+
+revoke update on table "public"."PERMITS" from "authenticated";
+
+revoke delete on table "public"."PERMITS" from "service_role";
+
+revoke insert on table "public"."PERMITS" from "service_role";
+
+revoke references on table "public"."PERMITS" from "service_role";
+
+revoke select on table "public"."PERMITS" from "service_role";
+
+revoke trigger on table "public"."PERMITS" from "service_role";
+
+revoke truncate on table "public"."PERMITS" from "service_role";
+
+revoke update on table "public"."PERMITS" from "service_role";
+
+revoke delete on table "public"."PROJECTS" from "anon";
+
+revoke insert on table "public"."PROJECTS" from "anon";
+
+revoke references on table "public"."PROJECTS" from "anon";
+
+revoke select on table "public"."PROJECTS" from "anon";
+
+revoke trigger on table "public"."PROJECTS" from "anon";
+
+revoke truncate on table "public"."PROJECTS" from "anon";
+
+revoke update on table "public"."PROJECTS" from "anon";
+
+revoke delete on table "public"."PROJECTS" from "authenticated";
+
+revoke insert on table "public"."PROJECTS" from "authenticated";
+
+revoke references on table "public"."PROJECTS" from "authenticated";
+
+revoke select on table "public"."PROJECTS" from "authenticated";
+
+revoke trigger on table "public"."PROJECTS" from "authenticated";
+
+revoke truncate on table "public"."PROJECTS" from "authenticated";
+
+revoke update on table "public"."PROJECTS" from "authenticated";
+
+revoke delete on table "public"."PROJECTS" from "service_role";
+
+revoke insert on table "public"."PROJECTS" from "service_role";
+
+revoke references on table "public"."PROJECTS" from "service_role";
+
+revoke select on table "public"."PROJECTS" from "service_role";
+
+revoke trigger on table "public"."PROJECTS" from "service_role";
+
+revoke truncate on table "public"."PROJECTS" from "service_role";
+
+revoke update on table "public"."PROJECTS" from "service_role";
+
+revoke delete on table "public"."ROLES" from "anon";
+
+revoke insert on table "public"."ROLES" from "anon";
+
+revoke references on table "public"."ROLES" from "anon";
+
+revoke select on table "public"."ROLES" from "anon";
+
+revoke trigger on table "public"."ROLES" from "anon";
+
+revoke truncate on table "public"."ROLES" from "anon";
+
+revoke update on table "public"."ROLES" from "anon";
+
+revoke delete on table "public"."ROLES" from "authenticated";
+
+revoke insert on table "public"."ROLES" from "authenticated";
+
+revoke references on table "public"."ROLES" from "authenticated";
+
+revoke select on table "public"."ROLES" from "authenticated";
+
+revoke trigger on table "public"."ROLES" from "authenticated";
+
+revoke truncate on table "public"."ROLES" from "authenticated";
+
+revoke update on table "public"."ROLES" from "authenticated";
+
+revoke delete on table "public"."ROLES" from "service_role";
+
+revoke insert on table "public"."ROLES" from "service_role";
+
+revoke references on table "public"."ROLES" from "service_role";
+
+revoke select on table "public"."ROLES" from "service_role";
+
+revoke trigger on table "public"."ROLES" from "service_role";
+
+revoke truncate on table "public"."ROLES" from "service_role";
+
+revoke update on table "public"."ROLES" from "service_role";
+
+revoke delete on table "public"."STATUSES" from "anon";
+
+revoke insert on table "public"."STATUSES" from "anon";
+
+revoke references on table "public"."STATUSES" from "anon";
+
+revoke select on table "public"."STATUSES" from "anon";
+
+revoke trigger on table "public"."STATUSES" from "anon";
+
+revoke truncate on table "public"."STATUSES" from "anon";
+
+revoke update on table "public"."STATUSES" from "anon";
+
+revoke delete on table "public"."STATUSES" from "authenticated";
+
+revoke insert on table "public"."STATUSES" from "authenticated";
+
+revoke references on table "public"."STATUSES" from "authenticated";
+
+revoke select on table "public"."STATUSES" from "authenticated";
+
+revoke trigger on table "public"."STATUSES" from "authenticated";
+
+revoke truncate on table "public"."STATUSES" from "authenticated";
+
+revoke update on table "public"."STATUSES" from "authenticated";
+
+revoke delete on table "public"."STATUSES" from "service_role";
+
+revoke insert on table "public"."STATUSES" from "service_role";
+
+revoke references on table "public"."STATUSES" from "service_role";
+
+revoke select on table "public"."STATUSES" from "service_role";
+
+revoke trigger on table "public"."STATUSES" from "service_role";
+
+revoke truncate on table "public"."STATUSES" from "service_role";
+
+revoke update on table "public"."STATUSES" from "service_role";
+
+revoke delete on table "public"."TIMESHEETS" from "anon";
+
+revoke insert on table "public"."TIMESHEETS" from "anon";
+
+revoke references on table "public"."TIMESHEETS" from "anon";
+
+revoke select on table "public"."TIMESHEETS" from "anon";
+
+revoke trigger on table "public"."TIMESHEETS" from "anon";
+
+revoke truncate on table "public"."TIMESHEETS" from "anon";
+
+revoke update on table "public"."TIMESHEETS" from "anon";
+
+revoke delete on table "public"."TIMESHEETS" from "authenticated";
+
+revoke insert on table "public"."TIMESHEETS" from "authenticated";
+
+revoke references on table "public"."TIMESHEETS" from "authenticated";
+
+revoke select on table "public"."TIMESHEETS" from "authenticated";
+
+revoke trigger on table "public"."TIMESHEETS" from "authenticated";
+
+revoke truncate on table "public"."TIMESHEETS" from "authenticated";
+
+revoke update on table "public"."TIMESHEETS" from "authenticated";
+
+revoke delete on table "public"."TIMESHEETS" from "service_role";
+
+revoke insert on table "public"."TIMESHEETS" from "service_role";
+
+revoke references on table "public"."TIMESHEETS" from "service_role";
+
+revoke select on table "public"."TIMESHEETS" from "service_role";
+
+revoke trigger on table "public"."TIMESHEETS" from "service_role";
+
+revoke truncate on table "public"."TIMESHEETS" from "service_role";
+
+revoke update on table "public"."TIMESHEETS" from "service_role";
+
+revoke delete on table "public"."USERS" from "anon";
+
+revoke insert on table "public"."USERS" from "anon";
+
+revoke references on table "public"."USERS" from "anon";
+
+revoke select on table "public"."USERS" from "anon";
+
+revoke trigger on table "public"."USERS" from "anon";
+
+revoke truncate on table "public"."USERS" from "anon";
+
+revoke update on table "public"."USERS" from "anon";
+
+revoke delete on table "public"."USERS" from "authenticated";
+
+revoke insert on table "public"."USERS" from "authenticated";
+
+revoke references on table "public"."USERS" from "authenticated";
+
+revoke select on table "public"."USERS" from "authenticated";
+
+revoke trigger on table "public"."USERS" from "authenticated";
+
+revoke truncate on table "public"."USERS" from "authenticated";
+
+revoke update on table "public"."USERS" from "authenticated";
+
+revoke delete on table "public"."USERS" from "service_role";
+
+revoke insert on table "public"."USERS" from "service_role";
+
+revoke references on table "public"."USERS" from "service_role";
+
+revoke select on table "public"."USERS" from "service_role";
+
+revoke trigger on table "public"."USERS" from "service_role";
+
+revoke truncate on table "public"."USERS" from "service_role";
+
+revoke update on table "public"."USERS" from "service_role";
+
+alter table "public"."ALLOCATIONS" drop constraint "ALLOCATIONS_project_id_fkey";
+
+alter table "public"."ALLOCATIONS" drop constraint "ALLOCATIONS_user_id_fkey";
+
+alter table "public"."PERMITS" drop constraint "PERMITS_reviewer_id_fkey";
+
+alter table "public"."PERMITS" drop constraint "PERMITS_user_id_fkey";
+
+alter table "public"."PROJECTS" drop constraint "PROJECTS_status_id_fkey";
+
+alter table "public"."TIMESHEETS" drop constraint "TIMESHEETS_project_id_fkey";
+
+alter table "public"."TIMESHEETS" drop constraint "TIMESHEETS_user_id_fkey";
+
+alter table "public"."USERS" drop constraint "USERS_auth_id_key";
+
+alter table "public"."USERS" drop constraint "USERS_email_key";
+
+alter table "public"."USERS" drop constraint "USERS_role_id_fkey";
+
+alter table "public"."ALLOCATIONS" drop constraint "ALLOCATIONS_pkey";
+
+alter table "public"."PERMITS" drop constraint "PERMITS_pkey";
+
+alter table "public"."PROJECTS" drop constraint "PROJECTS_pkey";
+
+alter table "public"."ROLES" drop constraint "ROLES_pkey";
+
+alter table "public"."STATUSES" drop constraint "STATUS_pkey";
+
+alter table "public"."TIMESHEETS" drop constraint "TIMESHEETS_pkey";
+
+alter table "public"."USERS" drop constraint "USERS_pkey";
+
+drop index if exists "public"."ALLOCATIONS_pkey";
+
+drop index if exists "public"."PERMITS_pkey";
+
+drop index if exists "public"."PROJECTS_pkey";
+
+drop index if exists "public"."ROLES_pkey";
+
+drop index if exists "public"."STATUS_pkey";
+
+drop index if exists "public"."TIMESHEETS_pkey";
+
+drop index if exists "public"."USERS_auth_id_key";
+
+drop index if exists "public"."USERS_email_key";
+
+drop index if exists "public"."USERS_pkey";
+
+drop table "public"."ALLOCATIONS";
+
+drop table "public"."PERMITS";
+
+drop table "public"."PROJECTS";
+
+drop table "public"."ROLES";
+
+drop table "public"."STATUSES";
+
+drop table "public"."TIMESHEETS";
+
+drop table "public"."USERS";
+
+create table "public"."allocation" (
+    "created_at" timestamp with time zone not null default now(),
+    "user_id" uuid not null,
+    "project_id" uuid not null,
+    "start_date" timestamp without time zone not null default now(),
+    "end_date" timestamp without time zone,
+    "percentage" smallint not null default '100'::smallint
+);
+
+
+alter table "public"."allocation" enable row level security;
+
+create table "public"."permit" (
+    "created_at" timestamp with time zone not null default now(),
+    "user_id" uuid not null,
+    "duration" numeric not null,
+    "id" uuid not null default gen_random_uuid(),
+    "description" text,
+    "type" text not null default 'Holiday'::text,
+    "start_date" timestamp without time zone not null,
+    "status" text not null default 'Waiting'::text,
+    "reviewer_id" uuid not null,
+    "end_date" timestamp without time zone
+);
+
+
+alter table "public"."permit" enable row level security;
+
+create table "public"."project" (
+    "id" uuid not null default gen_random_uuid(),
+    "created_at" timestamp with time zone not null default now(),
+    "name" character varying not null,
+    "client" character varying not null,
+    "description" text,
+    "start_date" timestamp without time zone not null,
+    "end_date" timestamp without time zone,
+    "status_id" uuid not null
+);
+
+
+alter table "public"."project" enable row level security;
+
+create table "public"."role" (
+    "id" uuid not null default gen_random_uuid(),
+    "created_at" timestamp with time zone not null default now(),
+    "label" character varying not null
+);
+
+
+alter table "public"."role" enable row level security;
+
+create table "public"."status" (
+    "id" uuid not null default gen_random_uuid(),
+    "created_at" timestamp with time zone not null default now(),
+    "name" character varying not null
+);
+
+
+alter table "public"."status" enable row level security;
+
+create table "public"."timesheet" (
+    "created_at" timestamp with time zone not null default now(),
+    "date" date not null,
+    "project_id" uuid not null,
+    "user_id" uuid not null,
+    "description" character varying,
+    "id" uuid not null default gen_random_uuid()
+);
+
+
+alter table "public"."timesheet" enable row level security;
+
+create table "public"."user" (
+    "id" uuid not null default gen_random_uuid(),
+    "created_at" timestamp with time zone not null default now(),
+    "email" text not null,
+    "name" text not null,
+    "avatar_url" text,
+    "role_id" uuid
+);
+
+
+alter table "public"."user" enable row level security;
+
+CREATE UNIQUE INDEX allocation_pkey ON public.allocation USING btree (user_id, project_id);
+
+CREATE UNIQUE INDEX permit_pkey ON public.permit USING btree (id);
+
+CREATE UNIQUE INDEX project_pkey ON public.project USING btree (id);
+
+CREATE UNIQUE INDEX role_pkey ON public.role USING btree (id);
+
+CREATE UNIQUE INDEX status_pkey ON public.status USING btree (id);
+
+CREATE UNIQUE INDEX timesheet_pkey ON public.timesheet USING btree (id);
+
+CREATE UNIQUE INDEX user_pkey ON public."user" USING btree (id);
+
+alter table "public"."allocation" add constraint "allocation_pkey" PRIMARY KEY using index "allocation_pkey";
+
+alter table "public"."permit" add constraint "permit_pkey" PRIMARY KEY using index "permit_pkey";
+
+alter table "public"."project" add constraint "project_pkey" PRIMARY KEY using index "project_pkey";
+
+alter table "public"."role" add constraint "role_pkey" PRIMARY KEY using index "role_pkey";
+
+alter table "public"."status" add constraint "status_pkey" PRIMARY KEY using index "status_pkey";
+
+alter table "public"."timesheet" add constraint "timesheet_pkey" PRIMARY KEY using index "timesheet_pkey";
+
+alter table "public"."user" add constraint "user_pkey" PRIMARY KEY using index "user_pkey";
+
+alter table "public"."allocation" add constraint "allocation_project_id_fkey" FOREIGN KEY (project_id) REFERENCES project(id) not valid;
+
+alter table "public"."allocation" validate constraint "allocation_project_id_fkey";
+
+alter table "public"."allocation" add constraint "allocation_user_id_fkey" FOREIGN KEY (user_id) REFERENCES "user"(id) not valid;
+
+alter table "public"."allocation" validate constraint "allocation_user_id_fkey";
+
+alter table "public"."permit" add constraint "permit_reviewer_id_fkey" FOREIGN KEY (reviewer_id) REFERENCES "user"(id) not valid;
+
+alter table "public"."permit" validate constraint "permit_reviewer_id_fkey";
+
+alter table "public"."permit" add constraint "permit_user_id_fkey" FOREIGN KEY (user_id) REFERENCES "user"(id) not valid;
+
+alter table "public"."permit" validate constraint "permit_user_id_fkey";
+
+alter table "public"."project" add constraint "project_status_id_fkey" FOREIGN KEY (status_id) REFERENCES status(id) not valid;
+
+alter table "public"."project" validate constraint "project_status_id_fkey";
+
+alter table "public"."timesheet" add constraint "timesheet_project_id_fkey" FOREIGN KEY (project_id) REFERENCES project(id) not valid;
+
+alter table "public"."timesheet" validate constraint "timesheet_project_id_fkey";
+
+alter table "public"."timesheet" add constraint "timesheet_user_id_fkey" FOREIGN KEY (user_id) REFERENCES "user"(id) not valid;
+
+alter table "public"."timesheet" validate constraint "timesheet_user_id_fkey";
+
+alter table "public"."user" add constraint "user_role_id_fkey" FOREIGN KEY (role_id) REFERENCES role(id) not valid;
+
+alter table "public"."user" validate constraint "user_role_id_fkey";
+
+
