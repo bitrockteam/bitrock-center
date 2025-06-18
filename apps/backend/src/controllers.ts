c08c8bf5ac8fe5168dfc6dc8287e51edea759e38
Davide Ghiotto <dghiotto.dev@gmail.com>
2025-06-18 18:01:56 +0200

    refactor: delete unused controllers be

--- apps/backend/src/controllers.ts
+++ apps/backend/src/controllers.ts
@@ -2,11 +2,6 @@ import { type Express, type Request, type Response } from "express";
 
 import { version } from "../package.json";
 import { createAiController } from "./controllers/ai.controller";
-import { createAllocationsController } from "./controllers/allocations.controller";
-import { createPermitsController } from "./controllers/permits.controller";
-import { createProjectsController } from "./controllers/projects.controller";
-import { createRolesController } from "./controllers/roles.controller";
-import { createTimesheetController } from "./controllers/timesheet.controller";
 import { createUserController } from "./controllers/user.controller";
 
 const isLocal = process.env.MODE === "local";
@@ -17,10 +12,5 @@ export const addPublicRoutes = (app: Express) => {
   });
 
   createUserController(app);
-  createRolesController(app);
-  createProjectsController(app);
-  createPermitsController(app);
-  createAllocationsController(app);
-  createTimesheetController(app);
   createAiController(app);
 };
