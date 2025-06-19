64beb417e78035eb90f48d1bf968f6a8d2b5235a
niccolonaso <niccolonaso@yahoo.it>
2025-06-19 10:35:40 +0200

    fix: only if the user has the permission show approval/reject permits table

--- apps/frontend/app/ferie-permessi/page.tsx
+++ apps/frontend/app/ferie-permessi/page.tsx
@@ -4,6 +4,7 @@ import PermitHistoryTable from "@/components/permits/permit-history-table";
 import PermitHeader from "@/components/permits/permit-header";
 import PermitApprovalTable from "@/components/permits/permit-approval";
 import { getUserInfoFromCookie } from "@/utils/supabase/server";
+import { fetchReferent } from "@/api/server/user/fetchReferent";
 
 export const metadata: Metadata = {
   title: "Ferie e Permessi | Bitrock Hours",
@@ -12,6 +13,7 @@ export const metadata: Metadata = {
 
 export default async function LeavePage() {
   const user = await getUserInfoFromCookie();
+  const usersListReferent = await fetchReferent();
   return (
     <div className="space-y-6">
       <PermitHeader />
@@ -22,9 +24,13 @@ export default async function LeavePage() {
         <div className="lg:col-span-2">
           <PermitHistoryTable />
         </div>
-        <div className="lg:col-span-3">
-          <PermitApprovalTable user={user} />
-        </div>
+        {(user.role === "Admin" ||
+          user.role === "Key_Client" ||
+          usersListReferent.length > 0) && (
+          <div className="lg:col-span-3">
+            <PermitApprovalTable user={user} />
+          </div>
+        )}
       </div>
     </div>
   );
