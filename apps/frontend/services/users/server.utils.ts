cf98c1d8462a2c4b10f76a4453821e2575d053bc
Davide Ghiotto <dghiotto.dev@gmail.com>
2025-06-19 17:49:08 +0200

    fix: allow roles

--- apps/frontend/services/users/server.utils.ts
+++ apps/frontend/services/users/server.utils.ts
@@ -5,5 +5,5 @@ import { Role } from "@bitrock/db";
 export async function allowRoles(roles: Role[]) {
   const user = await getUserInfoFromCookie();
   if (!user) return false;
-  return roles.includes(user.role);
+  return roles.find((r) => r === user.role);
 }
