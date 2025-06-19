c52435c50b9b76c816c60c19863b78a3b8673ffe
Davide Ghiotto <dghiotto.dev@gmail.com>
2025-06-19 17:37:08 +0200

    feat: use user hook

---
+++ apps/frontend/hooks/useUser.tsx
@@ -0,0 +1,28 @@
+import { getUserInfoFromCookie } from "@/utils/supabase/server";
+import { user } from "@bitrock/db";
+import { useEffect, useState } from "react";
+import { useUserPermissions } from "./useUserPermissions";
+
+export const useUser = () => {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState<user | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      setLoading(true);
+      const userInfo = await getUserInfoFromCookie();
+      setUser(userInfo);
+      setLoading(false);
+    };
+
+    fetchUser();
+  }, []);
+
+  const permissions = useUserPermissions();
+
+  return {
+    user,
+    loading,
+    permissions,
+  };
+};
