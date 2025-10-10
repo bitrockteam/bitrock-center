36548aca2f6f048931cadba048a1420dffa13437
maxattianese <maxattianesekr94@gmail.com.com>
2025-10-10 08:25:40 +0200

    fix: loader styles in light mode

---
+++ components/custom/loader/Loader.tsx
@@ -0,0 +1,9 @@
+export function Loader() {
+  return (
+    <div
+      className={`flex justify-center items-center h-full w-full bg-transparent`}
+    >
+      <div className="loader" />
+    </div>
+  );
+}
