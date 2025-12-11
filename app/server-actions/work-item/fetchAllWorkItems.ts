ddb46fd33e18463757b317b8bf760c701d6aef98
Niccolò Naso <niccolo.naso@bitrock.it>
2025-12-11 17:46:13 +0100

    fix: handle empty timesheets in fetchUserTimesheetById and update next version in package.json

--- app/server-actions/work-item/fetchAllWorkItems.ts
+++ app/server-actions/work-item/fetchAllWorkItems.ts
@@ -5,9 +5,8 @@ import { getUserInfoFromCookie } from "@/utils/supabase/server";
 
 export async function fetchAllWorkItems(params?: string | null) {
   const user = await getUserInfoFromCookie();
-
   // Example: restrict to user's enabled work items if not admin
-  if (user.role !== "Admin" && user.role !== "Super Admin") {
+  if (user.permissions.find((perm) => perm === "CAN_SEE_WORK_ITEM") == null) {
     return db.work_items.findMany({
       where: {
         work_item_enabled_users: {
